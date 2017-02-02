80352fe794e4009bb9e998e5b9331b5337a228a3
packowitz <PhilippSchum@web.de>
2017-02-02 18:11:42 +1300

    security surveys moved to new ionic version

--- src/pages/securitySurveys/security-surveys.component.ts
+++ src/pages/securitySurveys/security-surveys.component.ts
@@ -1,8 +1,14 @@
 import {Component} from "@angular/core";
+import {Survey} from "../../providers/domain/survey.model";
+import {SurveyService} from "../../providers/services/survey.service";
 
 @Component({
   templateUrl: 'security-surveys.component.html'
 })
 export class SecuritySurveyPage {
+  surveys: Survey[];
 
+  constructor(private surveyService: SurveyService) {
+    surveyService.listSecuritySurveys().subscribe(data => this.surveys = data);
+  }
 }
--- src/providers/services/survey.service.ts
+++ src/providers/services/survey.service.ts
@@ -1,10 +1,14 @@
 import {AtpHttp} from "./atp-http.service";
 import {Injectable} from "@angular/core";
+import {Observable} from "rxjs";
+import {Survey} from "../domain/survey.model";
 
 @Injectable()
 export class SurveyService {
   constructor(private atpHttp: AtpHttp) {
   }
 
-
+  listSecuritySurveys(): Observable<Survey[]> {
+    return this.atpHttp.doGet("/web/app/survey/security/list", "loading security ATPs");
+  }
 }
