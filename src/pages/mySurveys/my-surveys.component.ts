629e691abb8ab36ccfb1fca7077d332871948b7d
packowitz <PhilippSchum@web.de>
2017-02-17 12:52:09 +1300

    list own surveys

--- src/pages/mySurveys/my-surveys.component.ts
+++ src/pages/mySurveys/my-surveys.component.ts
@@ -1,8 +1,39 @@
 import {Component} from "@angular/core";
+import {NavController} from "ionic-angular";
+import {SurveyService} from "../../providers/services/survey.service";
+import {CreateSurveyPage} from "../createSurvey/create-survey.component";
+import {LocalStorage} from "../../providers/services/local-storage.service";
+import {Util} from "../../providers/services/util.service";
 
 @Component({
   templateUrl: 'my-surveys.component.html'
 })
 export class MySurveysPage {
+
+  constructor(public surveyService: SurveyService,
+              public nav: NavController,
+              public localStorage: LocalStorage,
+              public util: Util) {
+  }
+
+  ionViewDidEnter() {
+    if(this.localStorage.updateTimestamp) {
+      this.surveyService.updateMySurveys();
+    } else {
+      this.surveyService.getMySurveys().subscribe(
+        data => {
+          this.localStorage.setMySurveys(data);
+        }
+      );
+    }
+  }
+
+  createSurvey() {
+    this.nav.push(CreateSurveyPage);
+  }
+
+  openMultiPictureSurveyDetails() {
+
+  }
 
 }
--- src/providers/services/survey.service.ts
+++ src/providers/services/survey.service.ts
@@ -3,10 +3,46 @@ import {Injectable} from "@angular/core";
 import {Observable} from "rxjs";
 import {Survey} from "../domain/survey.model";
 import {SurveyType} from "../domain/survey-type.model";
+import {LocalStorage} from "./local-storage.service";
+
+export class SurveyListWithTimestamp {
+  data: Survey[];
+  timestamp: number;
+}
 
 @Injectable()
 export class SurveyService {
-  constructor(private atpHttp: AtpHttp) {
+  constructor(public atpHttp: AtpHttp, public localStorage: LocalStorage) {
+  }
+
+  getMySurveys(): Observable<SurveyListWithTimestamp> {
+    return this.atpHttp.doGet("/web/app/survey/own/list", "loading ATPs");
+  }
+
+  getUpdatesForMySurveysSince(timestamp: number): Observable<SurveyListWithTimestamp> {
+    return this.atpHttp.doGet("/web/app/survey/own/updates/since/" + timestamp, "updating ATPs");
+  }
+
+  updateMySurveys() {
+    this.getUpdatesForMySurveysSince(this.localStorage.updateTimestamp).subscribe(
+      data => {
+        let unknownSurveys: number[] = this.localStorage.updateMySurveys(data);
+        console.log("Updated " + data.data.length + " surveys");
+        if(unknownSurveys.length > 0) {
+          this.getMySurveysByIds(unknownSurveys).subscribe(
+            data => {
+              this.localStorage.addSurveys(data);
+              console.log("Loaded " + data.length + " new surveys");
+            }
+          );
+        }
+      }
+    );
+  }
+
+  getMySurveysByIds(ids: number[]): Observable<Survey[]> {
+    let idString = ids.join(',');
+    return this.atpHttp.doGetBackground("/web/app/survey/own/list/byids/" + idString);
   }
 
   listSecuritySurveys(): Observable<Survey[]> {
@@ -22,7 +58,7 @@ export class SurveyService {
   }
 
   deleteSurvey(survey: Survey): Observable<any> {
-    return this.atpHttp.doDelete("/web/app/survey/security/" + survey.id, "deleting ATP");
+    return this.atpHttp.doDelete("/web/app/survey/" + survey.id, "deleting ATP");
   }
 
   createSurvey(survey: Survey, type: SurveyType, pictures: string[], eachCountrySeparate: boolean): Observable<Survey[]> {
